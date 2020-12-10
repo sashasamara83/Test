98278b33eac98fdcbbf9a55104a3ba3bf31d2d41
Alexander <sashasamara83@mail.ru>
2020-12-10 04:23:51 -0800

    HelloWorld > a greater than 5

--- Project/HelloWorld.cpp
+++ Project/HelloWorld.cpp
@@ -5,7 +5,7 @@ int main() {
 	int a;
 	a = 5;
 	if (a>7) {
-	   cout << "HelloWorld 1" << endl;
+	   cout << "a greate than 5" << endl;
 	   system ("PAUSE");
 	} else {
 	   cout <<  "a less than 7" << endl;

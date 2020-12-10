955ecbfe685f28c4fb1f0e69f23e22f7e673f853
Alexander <sashasamara83@mail.ru>
2020-12-10 01:56:15 -0800

    added if-else

--- Project/HelloWorld.cpp
+++ Project/HelloWorld.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main() {
-	cout << "HelloWorld 1" << endl;
-	system ("PAUSE");
-
+	int a;
+	a = 5;
+	if (a>7) {
+	   cout << "HelloWorld 1" << endl;
+	   system ("PAUSE");
+	} else {
+	   cout <<  "a less than 7" << endl;
+	   system ("PAUSE");
+	  }
 	return 0;
 }
